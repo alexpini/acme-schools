b0b0162b49b129cf0df631caa0db1ba979856570
tomweidner103 <tomweidner103@gmail.com>
2019-10-07 15:42:19 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import {render} from 'react-dom'
 import thunks from 'redux-thunk'
 import { HashRouter, Link, Route } from 'react-router-dom'
 import { Provider, connect } from 'react-redux'
-
-const { Component } = React;
+const { Component } = React
+//todo
+//make Nav component
 
 class App extends React.Component {
   constructor() {
@@ -12,9 +13,15 @@ class App extends React.Component {
     this.state
   }
   render() {
-    return <div>Hello How Are You</div>
+    //load the _Nav component(below) here and Nav component(which hasnt been made yet, but will contain the navlinks
+    return
+      <div>
+        <Nav />
+        <_Nav />
+      </div> 
   }
 }
+
 
 class _Nav extends Component{
   componentDidMount(){
@@ -22,16 +29,20 @@ class _Nav extends Component{
   }
   render(){
     return (
+      //take out this hashrouter
       <HashRouter>
         <Route component={ Nav } />
+        //add <Switch>
         <Route path='/students' component={ Student } />
         <Route path='/student/create' component={ Create } />
         <Route path='/schools' component={ School } />
+        // add</Switch>
+      //take outthis too
       </HashRouter>
     );
   }
  }
 
-
+//your getElementById is from index.html whatever the div is with the id, maybe root?
 ReactDOM.render(<Provider store={store}><HashRouter><App /> </HashRouter></Provider>, document.getElementById(''))
 
